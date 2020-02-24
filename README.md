eb9efc9b4c078bc0715dbd779e28d66a073a789f
a.atalla <a.3talla@gmail.com>
2020-02-24 11:54:10 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Elm TASKS
+
+- This is my first very basic app with [elm](https://elm-lang.org/) using [create-elm-app](https://github.com/halfzebra/create-elm-app)
+- I decided to use normal css so i can focus only on Elm
+
+## Screenshot
+
+![Elm Tasks](https://user-images.githubusercontent.com/536140/75142492-e19a7e80-56fb-11ea-9357-fb292f8ef633.png)
---
+++ src/Main.elm
@@ -0,0 +1,118 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, h1, i, input, li, text, ul)
+import Html.Attributes exposing (class, classList, placeholder, style, target, value)
+import Html.Events exposing (onClick, onInput)
+
+
+
+---- MODEL ----
+
+
+type alias Task =
+    { id : Int
+    , description : String
+    , isComplete : Bool
+    }
+
+
+type alias Model =
+    { task : Task
+    , tasks : List Task
+    }
+
+
+initTask : Task
+initTask =
+    { id = 0, description = "", isComplete = False }
+
+
+init : Model
+init =
+    { task = initTask, tasks = [] }
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = TEXT_CHANGE String
+    | ADD_TASK
+    | TOGGLE_TASK Int
+    | DELETE_TASK Int
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        TEXT_CHANGE text ->
+            { model | task = { id = model.task.id, description = text, isComplete = False } }
+
+        ADD_TASK ->
+            let
+                oldTask =
+                    model.task
+
+                newTask =
+                    { oldTask | id = oldTask.id + 1 }
+            in
+            if model.task.description == "" then
+                model
+
+            else
+                { model
+                    | tasks =
+                        newTask
+                            :: model.tasks
+                    , task = { newTask | description = "" }
+                }
+
+        TOGGLE_TASK taskId ->
+            let
+                toggle task =
+                    if taskId == task.id then
+                        { task | isComplete = not task.isComplete }
+
+                    else
+                        task
+            in
+            { model | tasks = List.map toggle model.tasks }
+
+        DELETE_TASK taskId ->
+            { model | tasks = List.filter (\task -> not (taskId == task.id)) model.tasks }
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    let
+        tasks =
+            List.reverse model.tasks
+    in
+    div [ class "container" ]
+        [ h1 [ class "title" ] [ text "Tasks List" ]
+        , div [ class "form" ]
+            [ input [ class "input", placeholder "New Task", value model.task.description, onInput TEXT_CHANGE ] []
+            , button [ class "btn-add", onClick ADD_TASK ] [ i [ class "fas fa-plus-circle fa-2x" ] [] ]
+            ]
+        , div [ class "" ] [ ul [] (List.map toLi tasks) ]
+        ]
+
+
+toLi : Task -> Html Msg
+toLi task =
+    div
+        [ class "row item-wrapper" ]
+        [ li [ classList [ ( "is-complete", task.isComplete ) ], onClick (TOGGLE_TASK task.id) ] [ text task.description ]
+        , button [ class "delete-btn", onClick (DELETE_TASK task.id) ] [ i [ class "far fa-trash-alt fa-2x" ] [] ]
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
